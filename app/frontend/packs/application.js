1313e47f61e802f3bc6fdf67118d556b6b98e0da
Nathan Huberty <nathan.huberty@orange.fr>
2018-09-02 12:08:20 +0200

    setup stimulus

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -14,3 +14,10 @@ import "components/pages/home";
 document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
 
 console.log("Hello World from Webpacker");
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
